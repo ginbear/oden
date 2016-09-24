b6811f54f1f7ab116e5254775b8d875b3e739894
R.Shimizu <rs@RShimizu-no-MacBook-Pro.local>
2016-09-25 03:11:25 +0900

    Install nginx

--- cookbooks/nginx/default.rb
+++ cookbooks/nginx/default.rb
@@ -12,7 +12,14 @@
 #                 └── dynamic.conf
 
 # recipe.rb
-package "nginx"
 
-remote_file "/etc/nginx/conf.d/static.conf"
-template "/etc/nginx/conf.d/dynamic.conf"
+package 'http://nginx.org/packages/centos/7/noarch/RPMS/nginx-release-centos-7-0.el7.ngx.noarch.rpm' do
+  not_if 'rpm -q nginx-release-centos-7-0.el7.ngx.noarch'
+end
+
+package "nginx" do
+  action :install
+end
+
+# remote_file "/etc/nginx/conf.d/static.conf"
+# template "/etc/nginx/conf.d/dynamic.conf"
